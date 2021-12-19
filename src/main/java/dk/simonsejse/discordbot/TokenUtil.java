9ceb9794e7ccb680d3328b4e7ce23f062eaa5986
simonsejse <simonwa01@gmail.com>
2021-12-19 03:03:01 +0100

    Update TokenUtil.java

--- src/main/java/dk/simonsejse/discordbot/TokenUtil.java
+++ src/main/java/dk/simonsejse/discordbot/TokenUtil.java
@@ -2,6 +2,6 @@ package dk.simonsejse.discordbot;
 
 
 public class TokenUtil {
-    public static final String TOKEN = "OTA2NzE5MzAxNzkxMjY4OTA0.YYcuJw.rx9BGIEYZiCKbxuCPDxsU0aLzLw";
-    public static final String WEATHER_TOKEN = "7b31f55737394dbca18153751211212";
+    public static final String TOKEN = "dasda.YYcuJw.dsa";
+    public static final String WEATHER_TOKEN = "dsa";
 }
